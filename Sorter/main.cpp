c344b769f7b3f729dac30702f740bb7161501e8c
Alexey Samorukov <alexsam97@me.com>
2015-02-16 23:13:33 +0300

    Initial Commit

---
+++ Sorter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorter
+//
+//  Created by Alexey Samorukov on 16/02/15.
+//  Copyright (c) 2015 Alexey Samorukov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
